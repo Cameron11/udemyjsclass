0234897732596da890e1184969f323bb59c16293
cameron11 <crynowecer11@gmail.com>
2017-12-19 21:15:19 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var name = "camran";
+var lastName = prompt('What is your last name');
+console.log(name);
+console.log(lastName);  
+console.log(`hello my name is ${name} ${lastName}`);
+alert(`Hello, my name is ${name} ${lastName}`);
